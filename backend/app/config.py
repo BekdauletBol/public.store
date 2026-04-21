40715d6f930df25aea4914132db82d68b8cf72e3
BekdauletBol <bekdauletmakemusic@gmail.com>
2026-04-21 23:44:22 +0500

    config.py

---
+++ backend/app/config.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    app_name: str = "FastAPI"
+    debug: bool = True
+    database_url: str = "sqlite:///./test.db"
+    cors_origins: list = [
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "http://localhost:5173",
+        "http://localhost:3000",
+    ]
+
+    static_dir: str = "static"
+    images_dir: str = "static/images"
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
